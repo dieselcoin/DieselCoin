09ff5391547789e5d44abbee8cd5947614894e0c
Diesel <Diesel@oil>
2014-01-21 12:14:53 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+DieselCoin (DZL)
+
+300 coins per block
+Subsidy halves every 4 years
+
+120 seconds between blocks
+Retarget every 30 blocks
+630720000 total coins
+
+Net Port: 32001
+RPC Port: 32002
---
+++ src/checkpoints.cpp
@@ -0,0 +1,63 @@
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Copyright (c) 2011-2012 Litecoin Developers
+// Copyright (c) 2013 DieselCoin Developers
+
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <boost/assign/list_of.hpp> // for 'map_list_of()'
+#include <boost/foreach.hpp>
+
+#include "checkpoints.h"
+
+#include "main.h"
+#include "uint256.h"
+
+namespace Checkpoints
+{
+    typedef std::map<int, uint256> MapCheckpoints;
+
+    //
+    // What makes a good checkpoint block?
+    // + Is surrounded by blocks with reasonable timestamps
+    //   (no blocks before with a timestamp after, none after with
+    //    timestamp before)
+    // + Contains no strange transactions
+    //
+    static MapCheckpoints mapCheckpoints =
+            boost::assign::map_list_of
+            (     0, uint256("0x7f8a8ceee4a05f5ee70edb05a64103f8a0cf0c1bddf6e1c3eb4fd7438b6ef92b"))
+			(     1, uint256("0xf99de2b7efcdd2df19f0b6e96d5878f912e0a4327b33af3845e5e93a1c515995"))
+			(     2, uint256("0xff6921c4a4673cfd9e5acbf3492ea1d73c69e52fecceb1f34a2d20d7a2a1c8a4"))
+            ;
+
+
+    bool CheckBlock(int nHeight, const uint256& hash)
+    {
+        if (fTestNet) return true; // Testnet has no checkpoints
+
+        MapCheckpoints::const_iterator i = mapCheckpoints.find(nHeight);
+        if (i == mapCheckpoints.end()) return true;
+        return hash == i->second;
+    }
+
+    int GetTotalBlocksEstimate()
+    {
+        if (fTestNet) return 0;
+        return mapCheckpoints.rbegin()->first;
+    }
+
+    CBlockIndex* GetLastCheckpoint(const std::map<uint256, CBlockIndex*>& mapBlockIndex)
+    {
+        if (fTestNet) return NULL;
+
+        BOOST_REVERSE_FOREACH(const MapCheckpoints::value_type& i, mapCheckpoints)
+        {
+            const uint256& hash = i.second;
+            std::map<uint256, CBlockIndex*>::const_iterator t = mapBlockIndex.find(hash);
+            if (t != mapBlockIndex.end())
+                return t->second;
+        }
+        return NULL;
+    }
+}
